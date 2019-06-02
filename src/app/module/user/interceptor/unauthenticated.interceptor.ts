125ba58983c1bb4cec9b3c685cb065eb03c8158a
benjamin-BEFOLE <befolebenjamin@yahoo.fr>
2019-06-02 12:32:37 +0200

    add: fonctionnalité de connexion et limitation d'accès aux <Routes> pour les clients non connectés

--- src/app/module/user/interceptor/unauthenticated.interceptor.ts
+++ src/app/module/user/interceptor/unauthenticated.interceptor.ts
@@ -5,10 +5,13 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 import { GlobalService } from 'src/app/common/global.service';
+import { AuthenticationService } from '../service/authentication.service';
 
 @Injectable()
 export class unauthenticatedInterceptor implements HttpInterceptor {
-    constructor(public global: GlobalService, private router: Router) { }
+    constructor(public global: GlobalService, 
+        private authService: AuthenticationService,
+        private router: Router) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         
@@ -17,6 +20,7 @@ export class unauthenticatedInterceptor implements HttpInterceptor {
             if (err.status === 401) {
                 console.log("Non authentifié!!!");
 
+                this.authService.logOut();
                 // Redirection vers la page de connexion
                 this.router.navigate([`/${this.global.domainAppUrl}/connexion`]);
             }
@@ -25,6 +29,7 @@ export class unauthenticatedInterceptor implements HttpInterceptor {
             if (err.status === 419 || err.status === 440) {
                 console.log("Session expirée!!!");
 
+                this.authService.logOut();
                 // Redirection vers la page de connexion
                 this.router.navigate([`/${this.global.domainAppUrl}/connexion`]);
             }
